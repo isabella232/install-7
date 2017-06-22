41b3c23f27155d3fb47ca9d0e585dca88efd101a
Andre Gardiner <andre@sirdre.com>
2017-06-22 17:53:37 -0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -179,7 +179,7 @@ if($_POST) {
 		
  
 	<!-- jQuery & JavaScript -->  
-	<script src="./assets/lib/js/jquery-1.11.1.min.js"></script>
+	<script src="./assets/js/jquery-1.11.1.min.js"></script>
 	<script src="./assets/js/jquery.validate.min.js"></script>
 	<script src="./assets/js/form-validation.js"></script> 
 
