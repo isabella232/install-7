e2fb3ee6c3b1f00ed4c790bc29b641e161f957d6
Andre Gardiner <andre@sirdre.com>
2017-06-22 00:20:17 -0600

    Add files via upload

---
+++ db/install.sql
@@ -0,0 +1,17 @@
+-- CIFullCalendar v3 SQL Dump
+-- version 3.5.5.0 
+-- http://www.cifullcalendar.com/v3
+--
+-- Host: localhost
+-- Generation Time: Jun 20, 2017 at 08:37 PM
+-- Server version: 5.6.30
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
